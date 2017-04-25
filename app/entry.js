1b3339404411e33bd57bd5293dd7a603a6de7861
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-04-25 07:26:42 -0500

    Move JS to bundle

--- app/entry.js
+++ app/entry.js
@@ -1,9 +1,28 @@
 import Vue from 'vue'
 import Typeahead from './Typeahead.vue'
 import VueResource from 'vue-resource'
+import * as EmailValidator from 'email-validator'
 
 Vue.use(VueResource);
 var app = new Vue({
     el: "#app",
-    components: {'typeahead': Typeahead}
+    components: { 'typeahead': Typeahead }
 });
+
+// This should be replaced with vue-validate
+window.registerEmail = () => {
+    var email = document.getElementById('user_email').value;
+
+    // check email entered
+    if (email.length === 0) {
+        alert("Enter email");
+        return;
+    }
+
+    if (!EmailValidator.validate(email)) {
+        alert("Enter valid email");
+        return;
+    }
+
+    // send to mailchimp and forward to confirmation page
+}
