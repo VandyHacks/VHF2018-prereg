29d375d1cd085e45e64b96e9780ac9beba2f6b8e
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-04-24 22:04:08 -0500

    Yet another Trie implementation

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const path = require('path');
 const express = require('express');
 const fs = require('fs');
-const TrieSearch = require('trie-search');
-const fuzzysearch = require('fuzzysearch');
+const createTrie = require('autosuggest-trie');
 const expressStaticGzip = require("express-static-gzip");
 
 const app = express();
@@ -18,35 +17,13 @@ const universities = JSON.parse(fs.readFileSync('universities.json'))
   .map(uni => {
     return { name: uni.name, addr: uni.city + ', ' + uni.state };
   });
-fs.writeFileSync('universities.addr.json', JSON.stringify(universities));
-const maxResults = 5;
+// fs.writeFileSync('universities.addr.json', JSON.stringify(universities));
 
-
-const ts = new TrieSearch('name', { 'min': 3, 'splitOnRegEx': false });
-universities.forEach(ts.add.bind(ts));
-
-/*
-app.get('/findunis', (req, res) => res.json(ts.get(req.query.q).slice(0, 15)));
-*/
+const trie = createTrie(universities, 'name');
 
 app.get('/findunis', (req, res) => {
-  console.time('findunis');
-  var query = req.query.q.toLowerCase();
-  if (query.length < 3) {
-    res.json([]);
-  } else {
-    var results = ts.get(query).slice(0, maxResults);
-    for (var i = 0; i < universities.length && results.length < maxResults; i++) { // Still the fastest after benchmarks
-      var uni = universities[i];
-      if (fuzzysearch(query, uni.name.toLowerCase())) {
-        if (!results.includes(uni)) {
-          results.push(uni);
-        }
-      }
-    }
-    res.json(results);
-  }
-  console.timeEnd('findunis');
+  var query = req.query.q;
+  res.json(trie.getMatches(query, { limit: 5 }));
 });
 
 const server = app.listen(app.get('port'), () => {
