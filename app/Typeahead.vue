f87f59a4ea8fa2fe13fffc409daac5f68120b2ae
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-04-25 01:27:27 -0500

    Override reset for typeahead

--- app/Typeahead.vue
+++ app/Typeahead.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <!-- the input field -->
-    <input type="text" placeholder="Name of university" autocomplete="off" v-model="query" @keydown.down="down" @keydown.up="up" @keydown.enter="hit" @keydown.esc="reset" @blur="reset" @input="update" />
+    <input type="text" 
+    placeholder="Name of university" 
+    autocomplete="off" 
+    v-bind:value="query" 
+    v-model="query" 
+    @keydown.down="down" 
+    @keydown.up="up" 
+    @keydown.enter="hit" 
+    @keydown.esc="reset" 
+    @blur="reset" 
+    @input="update" />
   
     <!-- the list -->
     <ul v-show="hasItems">
@@ -17,18 +27,21 @@
 import VueTypeahead from 'vue-typeahead'
 
 export default {
-  extends: VueTypeahead, // vue@1.0.22+
-
+  extends: VueTypeahead,
   data: () => ({
-      src: '/findunis'
+    src: '/findunis',
+    selected: ''
   }),
-
   methods: {
     // The callback function which is triggered when the user hits on an item
     // (required)
     onHit(item) {
-      alert("City " + item.addr)
+      this.query = item.name;
     },
+    reset() {
+      this.items = [];
+      this.loading = false;
+    }
   }
 }
 </script>
