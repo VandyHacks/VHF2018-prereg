6027173d7d662108801221b29e87157bd09e373e
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-04-26 22:33:54 -0500

    Scale in content

--- app/entry.js
+++ app/entry.js
@@ -3,6 +3,8 @@ import Typeahead from './Typeahead.vue'
 import VueResource from 'vue-resource'
 import * as EmailValidator from 'email-validator'
 
+// Chrome doesn't handle single-loop GIFS properly, 
+// so opacity needs to be set on element
 setTimeout(() => {
         var logoEl = document.getElementById('logo');
         logoEl.style.opacity = '1';
@@ -10,6 +12,7 @@ setTimeout(() => {
         logoEl.src = '#';
         logoEl.src = imageUrl;
         document.getElementById('signup').style.opacity = '1';
+        document.getElementById('app').style.transform = 'scale(1)';
     }, 5);
 
 Vue.use(VueResource);
