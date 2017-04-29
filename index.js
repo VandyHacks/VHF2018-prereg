d3cbc18ec9e601181520d898ed4c0a0ffa004dc3
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-04-29 15:23:11 -0500

    Switch to compression module

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const path = require('path');
 const express = require('express');
 const fs = require('fs');
-const createTrie = require('autosuggest-trie');
-const expressStaticGzip = require("express-static-gzip");
+const compression = require('compression')
+const compressible = require('compressible');
 
 const app = express();
+app.use(compression({ filter: shouldCompress }));
+
+function shouldCompress (req, res) {
+  var type = res.getHeader('Content-Type')
+  // compressible doesn't think GIF should be compressed, which is wrong
+  if (type === undefined || (type != 'image/gif' && !compressible(type))) {
+    return false;
+  }
+  return true;
+}
+
 app.set('port', (process.env.PORT || 5000));
 
-app.use(expressStaticGzip(path.join(__dirname, 'public'), {
-	useBrotli: true
-}));
+app.use(express.static('public'));
 
 const server = app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
