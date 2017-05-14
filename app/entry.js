2cd8ccb3271dc1d522fcf4eb09244ced936380e8
Max Engel <maxengel99@gmail.com>
2017-05-14 04:21:14 -0500

    mailchimp endpoint and email subscription --> needs more work

--- app/entry.js
+++ app/entry.js
@@ -34,7 +34,87 @@ var app = new Vue({
     },
     methods: {
         registerEmail() {
-            console.log('registerEmail called...');
+          // grab email from input
+          var email = document.getElementById("email").childNodes[0].childNodes[0].value;
+          console.log('registerEmail test...');
+
+          // test to see if endpoint.php was reached
+          /*$.ajax({
+            url: "endpoint.php",
+            success: function(){
+              console.log("works for real");
+            },
+            error: function(){
+              console.log("fail");
+            }
+          })*/
+
+          // store data to send to Mailchip
+          // modified from http://nobleintentstudio.com/blog/mailchimp-api-example/
+          /*var data = {
+            email: email,
+            status: "subscribed"
+          };
+
+          // endpoint file
+          var endpoint = "endpoint.php";
+          console.log(data);
+          console.log(endpoint);
+
+          //make the ajax request
+          $.ajax({
+            method: 'POST',
+            dataType: "json",
+            url: "js/endpoint.php",
+            data: data,
+            success: function(data){
+              if(data.id){
+                //successful adds will have an id attribute on the object
+                alert('thanks for signing up');
+              } else if (data.title == 'Member Exists') {
+                alert('thanks, but you are alredy signed up');
+              } else {
+                //something went wrong with the API call
+                alert('oh no, there has been a problem (api call)');
+              }
+            },
+            error: function(){
+              alert('oh no, there has been a problem (server problem)');
+            }
+          });*/
+
+          // old code that tried to send information w/o php (not allowed
+          // for security reasons from mailchimp)
+          /*$(document).ready(function(){
+            var mcForm = $('#mailchimpForm');
+            var dc = 'us9';
+            var id = '5a2fc7077d';
+            var url = '//' + dc + '.api.mailchimp.com/3.0/list/' + id + '/members/';
+            var key = "2c364b1b877fda458de3b00f5410b3a3-us9";
+            var data = {
+              "email_address": email,
+              "status": "subscribed"
+            };
+
+            var params = JSON.stringify(data);
+
+            $.ajax({
+              url: url,
+              method: 'POST',
+              data: params,
+              dataType: 'json',
+              contentType: 'application/json; charset=utf-8',
+              beforeSend: function(xhr){
+                xhr.setRequestHeader("Authorization", "Basic" + btoa("api:" + key));
+              },
+              error: function(res, text){
+                alert('error');
+              },
+              success: function(res){
+                alert('error');
+              }
+            });
+          });*/
         }
     }
 });
--- index.js
+++ index.js
@@ -4,8 +4,49 @@ const fs = require('fs');
 const compression = require('compression')
 const compressible = require('compressible');
 
+// maybe delete
+var request = require('superagent');
+var bodyParser = require('body-parser');
+
 const app = express();
 app.use(compression({ filter: shouldCompress }));
+
+// added later
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+var mailchimpInstance   = 'us9',
+    listUniqueId        = '5a2fc7077d',
+    mailchimpApiKey     = '2c364b1b877fda458de3b00f5410b3a3-us9';
+
+app.post('/signup', function (req, res) {
+    request
+        .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
+        .set('Content-Type', 'application/json;charset=utf-8')
+        .set('Authorization', 'Basic ' + new Buffer('any:' + mailchimpApiKey ).toString('base64'))
+        .send({
+          'email_address': "anotheremail@gmail.com",
+          'status': 'subscribed',
+          'merge_fields': {
+            'FNAME': "first_name",
+            'LNAME': "last_name"
+          }
+        })
+            .end(function(err, response) {
+              if (response.status < 300 || (response.status === 400 && response.body.title === "Member Exists")) {
+                res.send('Signed Up!');
+              } else if(response.status === 400) {
+                res.send("400");
+              } else if(response.status === 401) {
+                res.send("401");
+              } else if(response.status == 404){
+                res.send("404");
+              } else {
+                res.send(response.status);
+              }
+            });
+});
 
 function shouldCompress (req, res) {
   var type = res.getHeader('Content-Type')
