45c9dbc39bfb33dc23de6fccb709b038b2bb3e8f
Varun Arora <varun.arora@vanderbilt.edu>
2017-05-20 14:24:55 -0700

    configured eslint and cleaned up code

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const path = require('path');
 const express = require('express');
-const fs = require('fs');
 const compression = require('compression')
 const compressible = require('compressible');
 
@@ -18,9 +16,9 @@ app.use(compression({ filter: shouldCompress }));
 app.use(bodyParser.json());
 
 // api var initialize
-var mailchimpInstance = process.env.MC_INSTANCE_ID,
-  listUniqueId = process.env.MC_LIST_ID,
-  mailchimpApiKey = process.env.MC_API_KEY;
+var mailchimpInstance = process.env.MC_INSTANCE_ID;
+var listUniqueId = process.env.MC_LIST_ID;
+var mailchimpApiKey = process.env.MC_API_KEY;
 
 var mailgunValidator = new MailgunValidator(process.env.MG_PUBLIC_KEY);
 
@@ -32,13 +30,14 @@ app.post('/signup', (req, res) => {
 
   mailgunValidator.validate(req.body.email, (err, result) => {
     if (err || !result.is_valid) {
+      if (err) console.log('ERROR: ' + err);
       res.json({ status: 'The submitted email was invalid. Please refresh and try again.' });
     } else {
       console.log('Registering ' + req.body.email + ' attending ' + req.body.university);
       request
         .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
         .set('Content-Type', 'application/json;charset=utf-8')
-        .set('Authorization', 'Basic ' + new Buffer('any:' + mailchimpApiKey).toString('base64'))
+        .set('Authorization', 'Basic ' + Buffer.from('any:' + mailchimpApiKey).toString('base64'))
         .send({
           'email_address': req.body.email,
           'status': 'pending',
@@ -47,14 +46,16 @@ app.post('/signup', (req, res) => {
           }
         })
         .end((err, response) => {
-          if (response.status < 300) {
+          if (!response || err) {
+            console.log(err);
+            res.json({ status: 'An unknown error occurred. Please refresh and try again.' });
+          } else if (response.status < 300) {
             res.json({ status: 'Thank you for pre-registering! We will announce more details soon.' });
           } else if (response.status === 400 && response.body.title === 'Member Exists') {
             res.json({ status: 'You have already pre-registered!' });
           } else if (response.status === 400 && response.body.title === 'Invalid Resource') {
             res.json({ status: 'That email address doesn\'t look real. Please refresh and try again.' });
           } else {
-            // console.log(response);
             res.json({ status: 'An unknown error occurred. Please refresh and try again.' });
           }
         });
@@ -63,9 +64,9 @@ app.post('/signup', (req, res) => {
 });
 
 function shouldCompress(req, res) {
-  var type = res.getHeader('Content-Type')
+  var type = res.getHeader('Content-Type');
   // compressible doesn't think GIF should be compressed, which is wrong
-  if (type === undefined || (type != 'image/gif' && !compressible(type))) {
+  if (type === undefined || (type !== 'image/gif' && !compressible(type))) {
     return false;
   }
   return true;
@@ -75,6 +76,6 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('public'));
 
-const server = app.listen(app.get('port'), () => {
+app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
 });
