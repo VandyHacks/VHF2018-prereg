1820df898a2c3578dac4edf3f0557ca022b3316f
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-05-21 16:02:35 -0600

    Prevent double submits

--- app/entry.js
+++ app/entry.js
@@ -29,7 +29,7 @@ new Vue({
       this.university = val;
     },
     submitRegistration() {
-      if (!this.areInputsValid) {
+      if (!this.areInputsValid || submitted) {
         return;
       }
       this.submitted = true;
