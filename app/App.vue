d69b0591a8c1fcf2c49c425bde3a1d0dae8fb721
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-05-21 19:29:55 -0600

    Remove unnecessary IDs

--- app/App.vue
+++ app/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <transition id="app" name="fade" mode="out-in">
-    <div id="signup" class="signup" v-if="!statusMessage" key="inputs">
+  <transition name="fade" mode="out-in">
+    <div class="signup" v-if="!statusMessage" key="inputs">
       <email :submitted="submitted" :email.sync="email" @pressed:enter="submitRegistration"></email>
       <typeahead :submitted="submitted" :query.sync="university" @pressed:enter="submitRegistration"></typeahead>
       <input type="submit" :value="submitted ? 'Sending...' : 'Get Notified'" :class="{ submitted: submitted }" :disabled="!submitAllowed" @click="submitRegistration">
